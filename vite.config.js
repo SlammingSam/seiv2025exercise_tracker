f8be9d173a558588fea0dcccd495252a38c10ff5
SlammingSam <samuel.harris@eagles.oc.edu>
2025-10-10 07:51:26 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vuetify from "vite-plugin-vuetify";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-import dns from "dns";
-dns.setDefaultResultOrder("verbatim");
-
-export default () => {
-  const baseURL =
-    process.env.APP_ENV === "development" ? "/" : "/tutorial-frontend-vue3/";
-
-  return defineConfig({
-    plugins: [vue(), vuetify({ autoImport: true })],
-
-    server: {
-      host: "localhost",
-      port: 8081,
-    },
-
-    base: baseURL,
-  });
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/seiv2025/p3/t4/'
+})
