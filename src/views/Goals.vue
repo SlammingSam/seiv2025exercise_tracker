82813184f27bccbe85ce5a7cec3c21426b36bbf3
joolean-equals-true <Julian.Moore7970@gmail.com>
2025-11-17 10:24:15 -0600

    fixed input tag... sorry :(

--- src/views/Goals.vue
+++ src/views/Goals.vue
@@ -140,9 +140,8 @@ async function addGoals(parsedList)
         color="#9d9e9d" 
         stroke-width="2"
         />
- </button>
-   
-        <input type="file" id="file-input" style="display:none;"></input>
+ </button> 
+    <input type="file" id="file-input" style="display:none;"/>
     <input type="text"  class = inputBetter v-model="input" placeholder="Search goals" />
 </div>
    
