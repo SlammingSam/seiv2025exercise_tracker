14c2435f68656ccb05600311b6af4cf6c8961652
joolean-equals-true <Julian.Moore7970@gmail.com>
2025-11-30 22:17:46 -0600

    changes to plan add

--- src/components/PlanView.vue
+++ src/components/PlanView.vue
@@ -3,12 +3,17 @@ import PlusIcon from "../components/plusIcon.vue";
 import SocialLogin from "../components/SocialLogin.vue";
 import exerciseServices from "../services/exercisesServices.js"
 import planServices from "../services/planServices.js"
-import PlanView from "../components/PlanView.vue"
 import { ref, onMounted } from "vue";
 const currentProgress = ref(75);
 const plans = ref([])
 const exercises = ref([])
 const message = ref("");
+
+const props = defineProps({
+  exercise_plan_id: { type: [Number, String]},
+  planName: { type: [Number, String]}
+})
+
 onMounted(() => {
   console.log("onMounted ran")
   getPlans();
@@ -41,33 +46,6 @@ async function getPlans(){
   }
 }
 
-let input = ref("");
-let arr = [];
-let deptArr = [];
-
-function filteredList() {
-  arr = [];
-  deptArr = [];
-
-  for (let name in plans.value) {
-    arr.push(plans.value[name]);
-    console.log(plans.value[name]);
-  } 
-
-  for (let el in arr) {
-    if(arr[el].name.toUpperCase().includes(input.value.toUpperCase())){
-      deptArr.push(arr[el]);
-    }
-  }
-  
-  if(input.value.toUpperCase() == ""){
-  return arr;
-  }else{ 
-    return deptArr;
-  }
-}
-
-
 const lists = ref([]);//list for the page display
 const parsedList = ref([]);//list to send to the database
 
@@ -81,51 +59,30 @@ function togglePlanView(){
   <v-container>
     <v-toolbar>
       <div class="home-header">
-      <p>Exercise Plans</p>
+      <p>{{ props.planName }}</p>
     </div>
     </v-toolbar>
-    <div class="flex-row-home">
-    <div class="left-header">
-      <p>My Plans</p>
-    </div>
-</div>
-<div class = "flex-row-search">
-    <input type="text"  class = inputBetter v-model="input" placeholder="Search Plans..." />
-</div>
-   
-  <div class = flex-row-table>
-      <button id = "plan-add" @click=" this.$router.push('/add-plan')">
-         <PlusIcon/>
-      </button>
-        <input type="file" id="plan-file-input" style="display:none;"/>
-    <table class ="long-table">
+
+         <table class ="long-table">
       <tbody class ="long-table">
-       
-         <tr>
-          <th>
-            Plan name
-          </th>
-          <th>
-            Description
-          </th>
+        <h3>Exercises</h3>
+        <tr class ="long-table">
+          <th>name</th>
+            <th>sets</th>
+            <th>reps</th>
+            <th>status</th>
         </tr>
-       
-           <tr v-for="item in filteredList()" :key="item.plan_id" class ="long-table">
-            <td>{{ item.name }}</td>
-            <td>{{ item.description }}</td>
-            <td><button @click="togglePlanView()">View</button></td>
+         <tr v-for="item in exercises" :key="item.exercise_id" class ="long-table">
+          <td v-if="item.role == 'Athlete' && item.exercise_plan_id == props.exercise_plan_id">{{ item.name }}</td>
+            <td>{{ item.sets }}</td>
+            <td>{{ item.reps }}</td>
+            <td>{{ item.status }}</td>
         </tr>
       </tbody>
-      </table>
-
-      <div>
-        <input type="file" id="exercise-file-input" style="display:none;"/>
-       
-      </div>
+    </table> 
  
-  </div>
   </v-container>
 
- 
+  <PlanView id = "PlanView"/>
 
 </template>
