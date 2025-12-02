a0ed3705ad122193c311fb501cd51b3c0191bf85
joolean-equals-true <160288072+joolean-equals-true@users.noreply.github.com>
2025-12-02 16:19:52 -0600

    plans can be assigned to teams and the schedule will appear!

---
+++ src/services/planAssignmentServices.js
@@ -0,0 +1,27 @@
+import apiClient from "./services.js";
+
+export default {
+  getAll() {
+    return apiClient.get("/plan_assignments");
+  },
+  getAllForUser(userId) {
+    //currently errors and will need to be modified to fit the schema
+    //return apiClient.get("/tutorials/userTut/" + userId);
+    return {};
+  },
+  get(id) {
+    return apiClient.get(`/plan_assignments/${id}`);
+  },
+  create(data) {
+    return apiClient.post("/plan_assignments", data);
+  },
+  update(id, data) {
+    return apiClient.put(`/plan_assignments/${id}`, data);
+  },
+  delete(id) {
+    return apiClient.delete(`/plan_assignments/${id}`);
+  },
+  deleteAll() {
+    return apiClient.delete(`/plan_assignments`);
+  },
+};
