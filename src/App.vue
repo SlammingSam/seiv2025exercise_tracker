af18d5afa21fa289ba62f43334da7ebe5553ec00
MetaphoricalAustin <157542034+MetaphoricalAustin@users.noreply.github.com>
2025-12-04 09:10:29 -0600

    Update menu display logic for user roles
    Refactor menu structure for logged-in users based on role.

--- src/App.vue
+++ src/App.vue
@@ -66,17 +66,21 @@ watch(userSession, (newUser) => {
         color="#9d9e9d" 
         stroke-width="3"
         />
-        <div id = "menu" class = "accordion-menu">
-    <h3><router-link :to="{ name: 'Home' }" v-if="isLoggedIn">Home</router-link></h3>
-     <h3><router-link :to="{ name: 'ExercisePlan' }" v-if="isLoggedIn">Exercise Plans</router-link></h3>
-      <h3><router-link :to="{ name: 'Goals' }" v-if="isLoggedIn">Goals</router-link></h3>
-       <h3><router-link :to="{ name: 'Profile' }" v-if="isLoggedIn">Profile</router-link></h3>
-        <h3><router-link :to="{ name: 'Teams' }" v-if="isLoggedIn && (isCoach || isAdmin)">My Teams</router-link></h3>
-         <h3><router-link :to="{ name: 'AdminPage' }" v-if="isLoggedIn && isAdmin">Admin</router-link></h3>
-         <h3><router-link :to="{ name: 'AthletePlan' }" v-if="isLoggedIn && (isCoach|| isAdmin)">Athlete Plans</router-link></h3>
-          <h3><router-link :to="{ name: 'AthletePlan' }" v-if="isLoggedIn">Plans</router-link></h3>
-            <h3><router-link :to="{ name: 'AddPlan' }" v-if="isLoggedIn">Add a plan</router-link></h3>
-            
+      <div id = "menu" class = "accordion-menu" v-if="isLoggedIn && !isCoach">
+       <h3><router-link :to="{ name: 'Home' }">Home</router-link></h3>
+        <h3><router-link :to="{ name: 'ExercisePlan' }">Exercise Plans</router-link></h3>
+         <h3><router-link :to="{ name: 'Goals' }">Goals</router-link></h3>
+          <h3><router-link :to="{ name: 'Profile' }">Profile</router-link></h3>
+            <h3><router-link :to="{ name: 'AddPlan' }">Add a plan</router-link></h3>
+      </div>
+      <div id = "menu" class = "accordion-menu" v-if="isLoggedIn && isCoach">
+       <h3><router-link :to="{ name: 'Home' }">Home</router-link></h3>
+        <h3><router-link :to="{ name: 'ExercisePlan' }">Exercise Plans</router-link></h3>
+         <h3><router-link :to="{ name: 'Goals' }">Goals</router-link></h3>
+          <h3><router-link :to="{ name: 'Profile' }">Profile</router-link></h3>
+           <h3><router-link :to="{ name: 'Teams' }">My Teams</router-link></h3>
+            <h3><router-link :to="{ name: 'AthletePlan' }">Athlete Plans</router-link></h3>
+             <h3><router-link :to="{ name: 'AddPlan' }">Add a plan</router-link></h3>
     </div>
     <v-main id="main">
       <router-view />
